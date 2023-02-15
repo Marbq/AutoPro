400ee9573e98d7f0fc4204c92704f4d3a6a3028d
CristianHebel <cristianhebelreyesrodriguez@alumno.ieselrincon.es>
2023-02-15 18:00:22 +0000

    TodoOK

--- Index/js/index.js
+++ Index/js/index.js
@@ -19,5 +19,23 @@ document.querySelector(".section-3-button2").addEventListener('click', function(
 document.querySelector(".center-button").addEventListener('click', function(){
     window.location.href = "subPagSobreNosotros.html";
 })
-
+/* Iniciar animaciones */
 AOS.init();
+
+/*Cambiar estilos cuando el menu hamburguesa este desplegado  */
+
+let navbarCollapse = document.getElementById("navbarCollapse");
+let text = document.querySelector(".text");
+
+
+navbarCollapse.addEventListener("show.bs.collapse", function() {
+  text.style.color = "white";
+  text.style.backgroundColor ="black";
+  text.style.opacity ="0.8";
+});
+
+navbarCollapse.addEventListener("hide.bs.collapse", function() {
+  text.style.color = "black";
+  text.style.backgroundColor ="transparent";
+  text.style.opacity ="1";
+});
